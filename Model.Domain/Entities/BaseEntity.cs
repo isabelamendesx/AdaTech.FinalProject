d65a5218d3c898137cefbf72c38a10f0d26e43a3
isabelamendesx <gomesisabela13@gmail.com>
2024-03-04 23:57:29 -0300

    feat: Add initial project structure using DDD design pattern

---
+++ Model.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public virtual uint Id {  get; set; } 
+    }
+}
---
+++ Model.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using Model.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Domain.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> filter = null!);
+        Task<T?> GetByParameter(Expression<Func<T, bool>> filter);
+        Task<bool> AddAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> Delete(T entity);
+    }
+}

efeda4f981425d9c78e05641540050bb06a3e376
suellensr <suellensreis@gmail.com>
2024-03-06 18:04:06 -0300

    feat: add status enum

---
+++ Model.Domain/Entities/EStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Domain.Entities
+{
+    internal enum EStatus
+    {
+        Submitted = 1,
+        Approved = 2,
+        Rejected = 3,
+        UnderEvaluation = 4,
+        Paid = 5
+    }
+}

a397e446dac7a0a98eb1a2850cd8791b5f9a1a30
angelar <angelar@ME003502>
2024-03-06 18:27:48 -0300

    feat: add RefundRequestDto

---
+++ Model.Domain/DTO/RefundRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Domain.DTO
+{
+    internal class RefundRequestDto
+    {
+        public string Description { get; set; }
+        public string Category { get; set;}
+        public decimal Total { get; set; }
+        public string Status { get; set; }
+    }
+}

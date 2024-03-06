562196e0df9642153887129f003eb46d00af54b6
suellensr <suellensreis@gmail.com>
2024-03-06 18:03:28 -0300

    feat: add category enum

---
+++ Model.Domain/Entities/ECategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Domain.Entities
+{
+    internal enum ECategory
+    {
+        Acomodation = 1,
+        Trasportation = 2,
+        Travel = 3,
+        Food = 4,
+        Others = 5
+    }
+}

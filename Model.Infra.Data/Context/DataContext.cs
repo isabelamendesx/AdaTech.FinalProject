d9b63d0985079a9f18046a439c2774e3efe4fdb1
AmandaaBastos <amandabastoscorreia@gmail.com>
2024-03-06 23:12:21 -0300

    feat: add DataContext

---
+++ Model.Infra.Data/Context/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Domain.Entities;
+
+
+namespace Model.Infra.Data.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Refund> Refunds { get; set; }
+
+    }
+}

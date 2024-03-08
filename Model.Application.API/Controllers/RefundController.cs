9120f7b82a7f97f551c9a017c6f984e7e61b76a3
isabelamendesx <gomesisabela13@gmail.com>
2024-03-08 13:27:45 -0300

    feat: add RefundController structure

---
+++ Model.Application.API/Controllers/RefundController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model.Service.Services;
+using Model.Domain.DTO;
+using Model.Domain.Entities;
+
+namespace Model.Application.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RefundController : ControllerBase
+    {
+        private readonly IRefundService _service;
+
+        public RefundController(IRefundService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public Task<IActionResult> CreateRefund([FromBody] RefundRequestDto request)
+        {
+
+        }
+
+        [HttpGet]
+        [Route("{status}")]
+        public Task<ActionResult<IEnumerable<Refund>>> GetAllByStatus(string status)
+        {
+
+        }
+
+        [HttpPost]
+        [Route("/approve/{id}")]
+        public Task<IActionResult> ApproveRefund(uint id)
+        {
+
+        }
+
+        [HttpPost]
+        [Route("/refuse/{id}")]
+        public Task<IActionResult> RefuseRefund(uint id)
+        {
+
+        }
+
+
+
+    }
+}

a2879ce24e17a8b340b585b5e82e238815f6bd66
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-03-08 11:42:52 -0300

    feat: new exceptions

---
+++ Model.Service/Exceptions/InternalErrorException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Service.Exceptions
+{
+    public class InternalErrorException : Exception
+    {
+        public int StatusCode { get; set; }
+        public InternalErrorException(string occasion) : base($"An internal error occured during: {occasion}")
+        {
+            StatusCode = 400;
+        }
+    }
+}

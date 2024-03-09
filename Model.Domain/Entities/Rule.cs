b0c265cea9bc0735d08f92f41d0a7c1f563b6fc4
Suellen S. Reis <suellensreis@gmail.com>
2024-03-09 15:53:40 -0300

    feat: Add Rule class

---
+++ Model.Domain/Entities/Rule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Domain.Entities
+{
+    public class Rule : BaseEntity
+    {
+        public int CategoryID { get; set; }
+        public decimal MinValue { get; set; }
+        public decimal MaxValue { get; set; }
+        public bool Action { get; set; }
+    }
+}

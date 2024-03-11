46112cbe06b9335cb77e37ee27ad2598d1ae1ed4
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-03-11 10:33:18 -0300

    feat: IRuleService interface

---
+++ Model.Service/Services/IRuleService.cs
@@ -0,0 +1,19 @@
+﻿using Model.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Service.Services
+{
+    public interface IRuleService
+    {
+        Task<Rule> CreateRule(Rule rule);
+        Task<IEnumerable<Rule?>> GetRulesToReproveAny();
+        Task<IEnumerable<Rule?>> GetRulesToApproveAny();
+        Task<IEnumerable<Rule?>> GetRulesToApproveByCategoryId(int categoryId);
+        Task<bool> DeactivateRule(int Id);
+        Task<bool> DeactivateACategorysRules(int categoryId);
+    }
+}

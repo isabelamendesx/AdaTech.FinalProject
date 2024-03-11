e282d198859df94f04f9e7a03098395d880de955
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-03-11 14:55:20 -0300

    fix: no need for a constructor

--- Model.Domain/Entities/Category.cs
+++ Model.Domain/Entities/Category.cs
@@ -2,10 +2,6 @@
 {
     public class Category : BaseEntity
     {
-        public Category(string name)
-        {
-            this.Name = name;
-        }
         public string Name { get; set; }
     }
 }

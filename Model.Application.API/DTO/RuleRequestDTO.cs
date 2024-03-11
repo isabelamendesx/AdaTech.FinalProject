cdc10f624edfe01bb0ccbcac05d4c4f9d03a3c20
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-03-11 13:56:08 -0300

    feat: rule request dto

---
+++ Model.Application.API/DTO/RuleRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿using Model.Application.API.DTO.Validators;
+using Model.Domain.Entities;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Model.Application.API.DTO
+{
+    public class RuleRequestDTO {
+        [NotNull]
+        public decimal MinValue { get; set; }
+        public decimal MaxValue { get; set; }
+        [ValidateRuleAction]
+        public string Action { get; set; }
+        [NotNull]
+        public uint CategoryId { get; set; }
+    
+    }
+}

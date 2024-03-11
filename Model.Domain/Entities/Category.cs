927948761f5e3a9f92e88b97d98941d81a76ee68
angelar <angelar@ME003502>
2024-03-11 14:11:29 -0300

    feat: Add Category Constructor

--- Model.Domain/Entities/Category.cs
+++ Model.Domain/Entities/Category.cs
@@ -2,6 +2,10 @@
 {
     public class Category : BaseEntity
     {
+        public Category(string name)
+        {
+            this.Name = name;
+        }
         public string Name { get; set; }
     }
 }

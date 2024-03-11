2411d3a05a5478d3f3b0a7478c8d74c7c961d67d
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-03-11 15:01:01 -0300

    feat: CategoryAlreadyRegisteredException

---
+++ Model.Service/Exceptions/CategoryAlreadyRegisteredException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Service.Exceptions
+{
+    internal class CategoryAlreadyRegisteredException : Exception
+    {
+        public int StatusCode { get; set; }
+        public CategoryAlreadyRegisteredException() : base("You can not create the same category twice.")
+        {
+            StatusCode = 400;
+        }
+    }
+}

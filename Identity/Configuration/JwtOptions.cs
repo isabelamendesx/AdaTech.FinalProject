7b10635bb4d653e8287444afa33d821e8ee123f6
Suellen S. Reis <suellensreis@gmail.com>
2024-03-12 01:04:08 -0300

    fix: Issue to Issuer

--- Identity/Configuration/JwtOptions.cs
+++ Identity/Configuration/JwtOptions.cs
@@ -9,7 +9,7 @@ namespace Identity.Configuration
 {
     public class JwtOptions
     {
-        public string Issue {  get; set; }
+        public string Issuer {  get; set; }
         public string Audience { get; set; }
         public SigningCredentials SigningCredentials { get; set; }
         public int Expiration {  get; set; }

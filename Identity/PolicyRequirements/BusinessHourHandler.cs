f7d3177c456dc57ba3e18891d9dea93c3afbc457
angelar <angelar@ME003502>
2024-03-12 08:34:59 -0300

    fix: JewtOptions Issue to Issuer

---
+++ Identity/PolicyRequirements/BusinessHourHandler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.PolicyRequirements
+{
+    internal class BusinessHourHandler: AuthorizationHandler<BusinessHoursRequirement>
+    {
+        protected override Task HandlerRequirementAsync(AuthorizationHandlerContext context, BusinessHoursRequirement requirement)
+        {
+            var currentTime = TimeOnly.FromDateTime(DateTime.Now);
+            if (currentTime.Hour >= 8 && currentTime <= 18)
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+    }
+}

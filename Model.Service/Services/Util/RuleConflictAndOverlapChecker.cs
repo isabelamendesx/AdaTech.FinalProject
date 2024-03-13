f10cf35f284943284c37479faa83704c37faec76
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-03-13 14:59:52 -0300

    feat: rule conflict and overlap checker

---
+++ Model.Service/Services/Util/RuleConflictAndOverlapChecker.cs
@@ -0,0 +1,66 @@
+﻿using Model.Domain.Entities;
+using Model.Service.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Service.Services.Util
+{
+    public static class RuleConflictAndOverlapChecker
+    {
+        public static void CheckForConflictAndOverlap(Rule newRule, List<Rule?> existingRules)
+        {
+            foreach(var rule in existingRules)
+            {
+                if(rule is not null)
+                {
+                    if (HasConflictingIntervals(rule, newRule))
+                    {
+                        if (rule.Action != newRule.Action)
+                            throw new RuleConflictException(rule.Id);
+                        else
+                            throw new RuleOverlapException(rule.Id);
+                    }
+                }
+            }
+        }
+
+        private static bool HasConflictingIntervals(Rule rule, Rule newRule)
+        {
+            if (IsRuleContainedWithin(rule, newRule))
+                return true;
+
+            if (IsRulePartiallyContainedWithin(rule, newRule))
+                return true;
+
+            return false;
+        }
+
+        private static bool IsRulePartiallyContainedWithin(Rule rule, Rule newRule)
+        {            
+            if (rule.MinValue <= newRule.MaxValue && newRule.MinValue <= rule.MaxValue)
+                return true;
+
+            if (newRule.MinValue <= rule.MaxValue && rule.MinValue <= newRule.MaxValue)
+                return true;
+
+            return false;
+        }
+
+        private static bool IsRuleContainedWithin(Rule rule, Rule newRule)
+        {
+            //new rule is contained within rule
+            if (rule.MinValue >= newRule.MinValue && rule.MaxValue >= newRule.MaxValue)
+                return true;
+
+            //rule is contained within new rule
+            if (newRule.MinValue >= rule.MinValue && newRule.MaxValue >= rule.MaxValue)
+                return true;
+
+            return false;
+        }
+
+    }
+}

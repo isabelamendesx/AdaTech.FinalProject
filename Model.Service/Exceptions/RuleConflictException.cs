b79a276e86c4b8e22e0bd4a5b44aa6ea59ca0a95
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-03-13 14:59:38 -0300

    feat: new exceptions for rules overlaps and conflicts

---
+++ Model.Service/Exceptions/RuleConflictException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Service.Exceptions
+{
+    public class RuleConflictException : Exception
+    {
+        public uint ConflictingRuleId { get; }
+        public int StatusCode { get; }
+        public string Details { get; }
+
+        public RuleConflictException(uint conflictingRuleId) 
+            : base("Cannot create a new rule if it conflicts with an active rule")
+        {
+            ConflictingRuleId = conflictingRuleId;
+            StatusCode = 404;
+            Details = "Please deactivate the conflicting rule or modify the new rule being created";
+        }
+    }
+}

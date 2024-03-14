05381f1ce6dca07eb11e9cb856dfda29827d221a
isabelamendesx <gomesisabela13@gmail.com>
2024-03-14 11:51:49 -0300

    refactor: update exception filter to include all custom exceptions

---
+++ Model.Application.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Model.Application.API.Filters.Model;
+using Model.Service.Exceptions;
+using Newtonsoft.Json;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Model.Application.API.Filters
+{
+    public sealed class ExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger<ExceptionFilter> _logger;
+
+        public ExceptionFilter(ILogger<ExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            var errorResultBuilder = new ErrorResponseBuilder()
+                .WithDefaultProperties(context);
+
+            if (context.Exception is RuleException ruleEx)
+                errorResultBuilder
+                   .WithConflictingRuleId(ruleEx.ConflictingRuleId);
+
+            var errorResult = errorResultBuilder.Build();
+            var ignoreNullFields = new JsonSerializerOptions { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull };
+
+            context.Result = new JsonResult(errorResult, ignoreNullFields)
+            {
+                StatusCode = errorResult.StatusCode
+            };
+
+            _logger.LogError(context.Exception, "Error caught in exception filter.");
+        }
+    }
+}

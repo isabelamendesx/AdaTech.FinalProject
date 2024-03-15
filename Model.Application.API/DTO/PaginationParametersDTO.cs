aefd018dab4ba61b3fb94a86057e97a7ded0b8da
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-03-14 22:49:07 -0300

    feat: pagination parameter dto

---
+++ Model.Application.API/DTO/PaginationParametersDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace Model.Application.API.DTO
+{
+    public class PaginationParametersDTO
+    {
+        const int MaxPageSize = 50;
+        private int _pageSize = 0;
+
+        public int PageNumber { get; set; } = 1;
+        public int PageSize
+        {
+            get { 
+                return _pageSize; 
+            }
+            set { 
+                _pageSize = (value > MaxPageSize) ? MaxPageSize : value; 
+            }
+        }
+    }
+}

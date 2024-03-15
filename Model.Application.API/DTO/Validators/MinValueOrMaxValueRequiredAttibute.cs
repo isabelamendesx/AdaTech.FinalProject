6173832a58ed02f504d887fdd9650790ef5f5c12
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-03-15 14:01:21 -0300

    fix: property might be null

--- Model.Application.API/DTO/Validators/MinValueOrMaxValueRequiredAttibute.cs
+++ Model.Application.API/DTO/Validators/MinValueOrMaxValueRequiredAttibute.cs
@@ -9,8 +9,15 @@ namespace Model.Application.API.DTO.Validators
             var minValueProperty = validationContext.ObjectType.GetProperty("MinValue");
             var maxValueProperty = validationContext.ObjectType.GetProperty("MaxValue");
 
-            var minValue = (decimal)minValueProperty.GetValue(validationContext.ObjectInstance, null);
-            var maxValue = (decimal)maxValueProperty.GetValue(validationContext.ObjectInstance, null);
+            if (minValueProperty == null && maxValueProperty == null)
+            {
+                return new ValidationResult("You must specify at least one of the range limits and it must be a valid decimal.");
+            }
+
+            var minValue = minValueProperty == null ? 0 : (decimal)minValueProperty.GetValue(validationContext.ObjectInstance, null);
+            var maxValue = maxValueProperty == null ? 0 : (decimal)maxValueProperty.GetValue(validationContext.ObjectInstance, null);
+
+           
 
             if(minValue == 0 && maxValue == 0)
                 return new ValidationResult("You must specify at least one of the range limits.");

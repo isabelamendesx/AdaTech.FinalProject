7968831df2ac326e446dba0abea573d08082e7bc
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-03-15 13:50:23 -0300

    feat: accept only valid ranges

--- Model.Application.API/DTO/Validators/MinValueOrMaxValueRequiredAttibute.cs
+++ Model.Application.API/DTO/Validators/MinValueOrMaxValueRequiredAttibute.cs
@@ -2,7 +2,7 @@
 
 namespace Model.Application.API.DTO.Validators
 {
-    public class MinValueOrMaxValueRequiredAttibute : ValidationAttribute
+    public class MinValueOrMaxValueValidatorAttibute : ValidationAttribute
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
@@ -14,6 +14,13 @@ namespace Model.Application.API.DTO.Validators
 
             if(minValue == 0 && maxValue == 0)
                 return new ValidationResult("You must specify at least one of the range limits.");
+
+            bool invalidRange = (minValue != 0 && maxValue != 0 && maxValue <= minValue) ||
+                (minValue != 0 && maxValue == 0 && minValue == decimal.MaxValue);
+                
+
+            if(invalidRange)
+                return new ValidationResult("You must specify a valid range.");
 
             return ValidationResult.Success;
         }

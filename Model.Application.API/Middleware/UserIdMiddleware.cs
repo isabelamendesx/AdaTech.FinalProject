e1b1ceefabbe8f11b41d326d0e3058c40cae8a34
isabelamendesx <gomesisabela13@gmail.com>
2024-03-16 16:52:03 -0300

    feat: create UserIdMiddleware

---
+++ Model.Application.API/Middleware/UserIdMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Claims;
+
+namespace Model.Application.API.Middleware
+{
+    public class UserIdMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public UserIdMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var identity = context.User.Identity as ClaimsIdentity;
+            var userId = identity?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            context.Items["UserId"] = userId;
+     
+            await _next(context);
+        }
+    }
+}

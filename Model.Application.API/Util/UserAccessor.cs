734ca2e2d5bfce23c53810407985bcc7d89fe1cc
Suellen S. Reis <suellensreis@gmail.com>
2024-03-16 14:37:44 -0300

    feat: add userAccessor class

---
+++ Model.Application.API/Util/UserAccessor.cs
@@ -0,0 +1,29 @@
+﻿using Model.Domain.Interfaces;
+using System.Security.Claims;
+
+namespace Model.Application.API.Util
+{
+    public class UserAccessor : IUserAccessor
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserAccessor(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetCurrentUserId()
+        {
+            var userIdentity = _httpContextAccessor.HttpContext.User.Identity;
+
+            if (userIdentity != null && userIdentity.IsAuthenticated)
+            {
+                var claimsIdentity = userIdentity as ClaimsIdentity;
+
+                return claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            }
+
+            return null;
+        }
+    }
+}

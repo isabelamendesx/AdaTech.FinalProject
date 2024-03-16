c54f4555e94d6a27795377009ed19027abe8f9e9
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-03-16 10:50:56 -0300

    feat: paged result class

---
+++ Model.Domain/Common/PagedResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Domain.Common
+{
+    public class PagedResult<T> where T : class
+    {
+        public int TotalCount { get; set; }
+        public IEnumerable<T> Items { get; set; }
+    }
+}

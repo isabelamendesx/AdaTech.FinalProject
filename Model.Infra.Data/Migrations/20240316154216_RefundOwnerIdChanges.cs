ab3dd5916db7f03bf5f527abd9a201f73ee102e8
Suellen S. Reis <suellensreis@gmail.com>
2024-03-16 13:10:50 -0300

    feat: add new migration and update database

---
+++ Model.Infra.Data/Migrations/20240316154216_RefundOwnerIdChanges.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Model.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefundOwnerIdChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefundOperations_Refunds_RefundId",
+                table: "RefundOperations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefundOperations_Rules_ApprovalRuleId",
+                table: "RefundOperations");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OwnerID",
+                table: "Refunds",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Refunds",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "Refunds",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn)
+                .OldAnnotation("Npgsql:IdentitySequenceOptions", "'100', '1', '', '', 'False', '1'")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "RefundId",
+                table: "RefundOperations",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApprovedBy",
+                table: "RefundOperations",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ApprovalRuleId",
+                table: "RefundOperations",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefundOperations_Refunds_RefundId",
+                table: "RefundOperations",
+                column: "RefundId",
+                principalTable: "Refunds",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefundOperations_Rules_ApprovalRuleId",
+                table: "RefundOperations",
+                column: "ApprovalRuleId",
+                principalTable: "Rules",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefundOperations_Refunds_RefundId",
+                table: "RefundOperations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefundOperations_Rules_ApprovalRuleId",
+                table: "RefundOperations");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "OwnerID",
+                table: "Refunds",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Refunds",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "Refunds",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .Annotation("Npgsql:IdentitySequenceOptions", "'100', '1', '', '', 'False', '1'")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn)
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "RefundId",
+                table: "RefundOperations",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ApprovedBy",
+                table: "RefundOperations",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ApprovalRuleId",
+                table: "RefundOperations",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefundOperations_Refunds_RefundId",
+                table: "RefundOperations",
+                column: "RefundId",
+                principalTable: "Refunds",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefundOperations_Rules_ApprovalRuleId",
+                table: "RefundOperations",
+                column: "ApprovalRuleId",
+                principalTable: "Rules",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

da217feba719f30e6effed930b99cb9157aeea3d
isabelamendesx <gomesisabela13@gmail.com>
2024-03-16 17:14:38 -0300

    refactor: adding GetTitle and GetStatusCode methods to ExceptionFilter

--- Model.Application.API/Filters/ExceptionFilter.cs
+++ Model.Application.API/Filters/ExceptionFilter.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.Filters;
 using Model.Application.API.Filters.Model;
 using Model.Service.Exceptions;
 using Newtonsoft.Json;
+using System.Net;
 using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -22,7 +23,9 @@ namespace Model.Application.API.Filters
         public void OnException(ExceptionContext context)
         {
             var errorResultBuilder = new ErrorResponseBuilder()
-                .WithDefaultProperties(context);
+                .WithDefaultProperties(context)
+                .WithTitle(GetTitle(context.Exception))
+                .WithStatusCode(GetStatusCode(context.Exception));
 
             if (context.Exception is RuleException ruleEx)
                 errorResultBuilder
@@ -43,5 +46,32 @@ namespace Model.Application.API.Filters
 
             _logger.LogError(context.Exception, "Error caught in exception filter.");
         }
+
+        private int GetStatusCode<T>(T ex) where T : Exception
+        {
+            var statusCodeMap = new Dictionary<Type, Func<int>>
+            {
+                { typeof(RuleException), () => (ex as RuleException)!.StatusCode },
+                { typeof(ResourceNotFoundException), () => (ex as ResourceNotFoundException)!.StatusCode},
+                { typeof(InternalErrorException), () => (ex as InternalErrorException)!.StatusCode },
+                { typeof(CategoryAlreadyRegisteredException), () => (ex as CategoryAlreadyRegisteredException)!.StatusCode},
+            };
+
+            return statusCodeMap.TryGetValue(ex.GetType(), out var statusCodeFunc) ? statusCodeFunc() : (int)HttpStatusCode.InternalServerError;
+        }
+
+        private string GetTitle<T>(T ex) where T : Exception
+        {
+            var titleMap = new Dictionary<Type, string>
+            {
+                { typeof(RuleException), "Rule Operation Error" },
+                { typeof(ResourceNotFoundException), "Resource Error" },
+                { typeof(InternalErrorException), "Internal Error" },
+                { typeof(CategoryAlreadyRegisteredException), "Category Operation Error" },
+            };
+
+            return titleMap.TryGetValue(ex.GetType(), out var title) ? title : "Internal Server Error";
+        }
+
     }
 }

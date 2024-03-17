50b5d336e175d8924ccba47cd666f011af4f6f26
isabelamendesx <gomesisabela13@gmail.com>
2024-03-17 00:18:01 -0300

    feat: implementing BaseController and ValidationWithDataAnnotation method

---
+++ Model.Application.API/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Model.Service.Exceptions;
+
+namespace Model.Application.API.Controllers
+{
+    public abstract class BaseController : ControllerBase
+    {
+        protected void ValidateWithDataAnotation()
+        {
+            if (!ModelState.IsValid)
+                throw new ValidationException(ModelState);
+        }
+
+    }
+}

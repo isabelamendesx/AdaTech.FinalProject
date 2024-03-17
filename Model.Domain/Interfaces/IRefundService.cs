b57e3a39da1e9cbc508ccdbf2f4510b175b6e6fc
isabelamendesx <gomesisabela13@gmail.com>
2024-03-17 15:08:48 -0300

    feat: add GetById in RefundService

--- Model.Domain/Interfaces/IRefundService.cs
+++ Model.Domain/Interfaces/IRefundService.cs
@@ -11,6 +11,7 @@ namespace Model.Domain.Interfaces
     public interface IRefundService
     {
         Task<Refund> CreateRefund(Refund refund, CancellationToken ct);
+        Task<Refund?> GetById(uint id, CancellationToken ct);
         Task<IEnumerable<Refund?>> GetAllByStatus(EStatus status, CancellationToken ct);
         Task<PaginatedResult<Refund>> GetAllByStatusPaginated(EStatus status, CancellationToken ct, int skip, int take);
         Task<Refund> ApproveRefund(uint Id, string userId, CancellationToken ct);
